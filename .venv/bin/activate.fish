87b26cf367b81a555dd97708e243af83f4c61a7f
Lori Henderson <lorim95@gmail.com>
2020-08-05 11:51:00 -0400

    Fixed TypeError

--- .venv/bin/activate.fish
+++ .venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/lorihenderson/django-projects/hello_django_main/.venv"
+set -gx VIRTUAL_ENV "/Users/lorihenderson/hello_django/.venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
